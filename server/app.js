92763d6fcd42fc4187fcf81d0588b411e11ac001
dominionism <cainglet@uw.edu>
2025-03-02 18:59:27 -0800

    Enabled the ability for the client side to fetch resources from the server side.

--- server/app.js
+++ server/app.js
@@ -2,16 +2,24 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import cors from 'cors';
 import usersRouter from './routes/users.js';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var app = express();
+
+/* Allows all domains to access the server's resources. */
+/* If we want to limit the domains that can access the server's
+   resources, we create a courseOptions variable and assign its
+   origin property a domain we want to specifically accept
+   requests from. */
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
