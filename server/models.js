cc033a8452527a467903a1f7125d25cd176f884e
dominionism <cainglet@uw.edu>
2025-03-04 14:08:54 -0800

    Made some slight changes.

--- server/models.js
+++ server/models.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 let models = {};
 
 async function main() {
-    console.log("Connecting to the restaurant databse...");
+    console.log("Connecting to the restaurant database...");
     await mongoose.connect('mongodb+srv://cainglet:restaurant-cluster@restaurantcluster.lsviq.mongodb.net/restaurizeDB?retryWrites=true&w=majority&appName=RestaurantCluster');
     console.log("Connected...");
 }
